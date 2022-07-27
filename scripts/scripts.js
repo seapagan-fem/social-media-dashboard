6d41b083006fd6cc62b1953498813111429daf2e
Grant Ramsay <seapagan@gmail.com>
2022-07-27 16:10:34 +0100

    choose start color mode based on  OS setting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,28 @@
 console.log("JavaScript loaded");
 
 const bodyEl = document.querySelector("body");
+const toggleEl = document.getElementById("toggle-colors");
+const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
 
 // add click listener to the checkbox to switch themes
 const handleToggleColors = (e) => {
   bodyEl.className = e.target.checked ? "theme-light" : "theme-dark";
 };
 
-document
-  .getElementById("toggle-colors")
-  .addEventListener("click", handleToggleColors);
+toggleEl.addEventListener("click", handleToggleColors);
 
-// apply dark mode by default
-bodyEl.className = "theme-dark";
+/* -------------------------------------------------------------------------- */
+/*                  detect dark/light mode on browsing device                 */
+/* -------------------------------------------------------------------------- */
+const setMode = (mode) => {
+  bodyEl.className = mode ? "theme-dark" : "theme-light";
+  toggleEl.checked = mode ? false : true;
+};
+
+// watch for changes to dark/light mode
+matchMedia.addEventListener("change", (e) => {
+  setMode(e.matches);
+});
+
+// set correct mode on refresh
+setMode(matchMedia.matches);
