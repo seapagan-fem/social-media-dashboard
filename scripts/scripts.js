048550a8d4c032b8c6d37c1efcc8d1d0af8c7cb9
Grant Ramsay <seapagan@gmail.com>
2022-07-27 12:14:59 +0100

    implement (unstyled) theme toggle

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
 console.log("JavaScript loaded");
+
+const bodyEl = document.querySelector("body");
+
+// add click listener to the checkbox to switch themes
+const handleToggleColors = (e) => {
+  bodyEl.className = e.target.checked ? "theme-light" : "theme-dark";
+};
+
+document
+  .getElementById("toggle-colors")
+  .addEventListener("click", handleToggleColors);
+
+// apply dark mode by default
+bodyEl.className = "theme-dark";
